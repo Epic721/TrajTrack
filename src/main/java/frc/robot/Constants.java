55308203eb5732d2a875510e2581391b3641adc0
Epic721 <karadrish@gmail.com>
2021-02-14 00:11:29 -0800

    Initial Commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,96 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static final class DriveConstants {
+    public static final int kLeftMotor1Port = 1;
+    public static final int kLeftMotor2Port = 2;
+    public static final int kRightMotor1Port = 3;
+    public static final int kRightMotor2Port = 4; 
+    
+    public static final int PigeonIMUPort = 0;
+
+    public static final TalonFXInvertType kLeftInvertType = TalonFXInvertType.Clockwise;
+    public static final TalonFXInvertType kRightInvertType = TalonFXInvertType.CounterClockwise;
+
+    public static final double kDriveSpeed = 0.8;
+
+    //void - need to delte (Falcon motors have integrated encoders)
+    /*public static final int[] kLeftEncoderPorts = new int[] {0, 1};
+    public static final int[] kRightEncoderPorts = new int[] {2, 3};
+    public static final boolean kLeftEncoderReversed = false;
+    public static final boolean kRightEncoderReversed = true;*/
+    //void
+
+    public static final double kTrackwidthMeters = 0.69; //27 inches
+    public static final DifferentialDriveKinematics kDriveKinematics =
+        new DifferentialDriveKinematics(kTrackwidthMeters);
+
+    public static final int kEncoderCPR = 2048; //https://docs.ctre-phoenix.com/en/latest/ch14_MCSensor.html
+    public static final double kWheelDiameterMeters = 0.15; //6 inches
+    public static final double kEncoderDistancePerPulse =
+        // Assumes the encoders are directly mounted on the wheel shafts
+        (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
+
+    // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
+    // These characterization values MUST be determined either experimentally or theoretically
+    // for *your* robot's drive.
+    // The Robot Characterization Toolsuite provides a convenient tool for obtaining these
+    // values for your robot. 
+    
+  
+    //THE CONSTANTS BELOW NEED TO BE TESTED FOR THE VOLTAGE EQUATION (*FEEDFORWARD CONSTANTS*)
+    public static final double ksVolts = 0.22;
+    public static final double kvVoltSecondsPerMeter = 1.98;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+
+    // Example value only - as above, this must be tuned for your drive!
+    public static final double kPDriveVel = 8.5; // THE "P" *FEEDBACK* CONSTANT
+  }
+
+  public static final class OIConstants {
+    public static final int kDriverControllerPort = 0;
+    public static final int kOperatorControllerPort = 1;
+
+    public static final Button kToggleQuickTurnButton = Button.kA;
+    public static final Button kIntakeButton = Button.kA;
+    public static final Button kStopRollerButton = Button.kX;
+    public static final Button kOuttakeButton = Button.kB;
+  }
+
+  public static final class AutoConstants {
+    public static final double kMaxSpeedMetersPerSecond = 3;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+
+    // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+    public static final double kRamseteB = 2;
+    public static final double kRamseteZeta = 0.7;
+  }
+
+  public final class IntakeConstants {
+    public static final int kArmPort = 5;
+    public static final int kRollerPort = 6;
+
+    public static final boolean kArmInvert = false;
+    public static final boolean kRollerInvert = false;
+
+    public static final double kIntakeSpeed = 0.8;
+    public static final double kOuttakeSpeed = 0.8;
+ }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,192 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PWMSparkMax;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Constants;
+import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+
+  
+  private final WPI_TalonFX leftMaster = new WPI_TalonFX(Constants.DriveConstants.kLeftMotor1Port);
+  private final WPI_TalonFX leftFollower = new WPI_TalonFX(Constants.DriveConstants.kLeftMotor2Port);
+  private final WPI_TalonFX rightMaster = new WPI_TalonFX(Constants.DriveConstants.kRightMotor1Port);
+  private final WPI_TalonFX rightFollower = new WPI_TalonFX(Constants.DriveConstants.kRightMotor2Port);
+
+  // The motors on the left side of the drive.
+  private final SpeedControllerGroup m_leftMotors = new SpeedControllerGroup(leftMaster, leftFollower);
+
+  // The motors on the right side of the drive.
+  private final SpeedControllerGroup m_rightMotors = new SpeedControllerGroup(rightMaster, rightFollower);
+
+  // The robot's drive
+  private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+
+  // The gyro sensor
+  private final PigeonIMU m_gyro = new PigeonIMU(Constants.DriveConstants.PigeonIMUPort);
+
+  // Odometry class for tracking robot pose
+  private final DifferentialDriveOdometry m_odometry;
+
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {
+
+    leftMaster.setSelectedSensorPosition(0);
+    rightMaster.setSelectedSensorPosition(0);
+
+    m_odometry = new DifferentialDriveOdometry(getRotation2dPdg());
+  }
+
+  @Override
+  public void periodic() {
+    // Update the odometry in the periodic block
+    m_odometry.update(
+        getRotation2dPdg(), leftMaster.getSelectedSensorPosition(), rightMaster.getSelectedSensorPosition());
+  }
+
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+  }
+
+  /**
+   * Returns the current wheel speeds of the robot.
+   *
+   * @return The current wheel speeds.
+   */
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(leftMaster.getSelectedSensorVelocity(), rightMaster.getSelectedSensorVelocity());
+  }
+
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    m_odometry.resetPosition(pose, getRotation2dPdg());
+  }
+
+  /**
+   * Drives the robot using arcade controls.
+   *
+   * @param fwd the commanded forward movement
+   * @param rot the commanded rotation
+   */
+  public void arcadeDrive(double fwd, double rot) {
+    m_drive.arcadeDrive(fwd, rot);
+  }
+
+  /**
+   * Controls the left and right sides of the drive directly with voltages.
+   *
+   * @param leftVolts the commanded left output
+   * @param rightVolts the commanded right output
+   */
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+    m_leftMotors.setVoltage(leftVolts);
+    m_rightMotors.setVoltage(-rightVolts);
+    m_drive.feed(); //FEEDFORWARD
+  }
+
+  /** Resets the drive encoders to currently read a position of 0. */
+  public void resetEncoders() {
+    leftMaster.setSelectedSensorPosition(0);
+    rightMaster.setSelectedSensorPosition(0);
+  }
+
+  /**
+   * Gets the average distance of the two encoders.
+   *
+   * @return the average of the two encoder readings
+   */
+  public double getAverageEncoderDistance() {
+    return (leftMaster.getSelectedSensorPosition() + rightMaster.getSelectedSensorPosition()) / 2.0;
+  }
+
+  /**
+   * Gets the left drive encoder.
+   *
+   * @return the left drive encoder
+   */
+
+   //WE DON'T NEED THIS
+  // public Encoder getLeftEncoder() {
+  //   return m_leftEncoder;
+  // }
+
+  /**
+   * Gets the right drive encoder.
+   *
+   * @return the right drive encoder
+   */
+
+  //WE DON'T NEED THIS
+  // public Encoder getRightEncoder() {
+  //   return m_rightEncoder;
+  // }
+
+  /**
+   * Sets the max output of the drive. Useful for scaling the drive to drive more slowly.
+   *
+   * @param maxOutput the maximum output to which the drive will be constrained
+   */
+  public void setMaxOutput(double maxOutput) {
+    m_drive.setMaxOutput(maxOutput);
+  }
+
+  /** Zeroes the heading of the robot. */
+  public void zeroHeading() {
+    m_gyro.setYaw(0);
+  }
+
+  /**
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from -180 to 180
+   */
+  public double getHeading() {
+    return getRotation2dPdg().getDegrees();
+  }
+
+  /**
+   * Returns the turn rate of the robot.
+   *
+   * @return The turn rate of the robot, in degrees per second
+   */
+  public double getTurnRate() {
+    return -getRatePdg(); //WHY NEGATIVE? MAYBE THE TURN RATE NEEDS TO BE INVERTED?
+  }
+
+  
+  //CUSTOM DEFINED FUNCTIONS (FOR PIGEON IMU)
+  public Rotation2d getRotation2dPdg(){
+    double YPR [] = new double [3];
+    m_gyro.getYawPitchRoll(YPR);
+    return Rotation2d.fromDegrees(YPR[0]);
+  }
+
+  public double getRatePdg(){
+    double XYZ [] = new double [3];
+    m_gyro.getRawGyro(XYZ);
+    return XYZ[0];
+  }
+
+}
