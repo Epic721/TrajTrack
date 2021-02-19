341181ac7dc7cdf38022b49ecae270f456aab52f
Epic721 <karadrish@gmail.com>
2021-02-18 16:33:41 -0800

    Corrected Motor Inversion

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -115,7 +115,7 @@ public class DriveSubsystem extends SubsystemBase {
    */
   public void tankDriveVolts(double leftVolts, double rightVolts) {
     m_leftMotors.setVoltage(leftVolts);
-    m_rightMotors.setVoltage(-rightVolts);
+    m_rightMotors.setVoltage(rightVolts);
     m_drive.feed(); 
   }
 
